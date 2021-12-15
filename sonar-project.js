dfcd54cd11c4aebed6fab5da36309a0f3a4f90f9
Wong Cheng Ngai <wcngai@peregrines-mbp.lan>
2021-12-16 04:53:17 +0800

    fix sonar json file

--- sonar-project.js
+++ sonar-project.js
@@ -1,8 +1,10 @@
 const sonarqubeScanner = require('sonarqube-scanner');
-  sonarqubeScanner({
-    serverUrl: 'http://192.168.56.101:9000',
-      options : {
-        'sonar.sources': '.',
-        //'sonar.inclusions' : '.' // Entry point of your code
-      }
-    }, () => {});
+
+sonarqubeScanner({
+  serverUrl: 'http://192.168.56.101:9000',
+    options : {
+      'sonar.sources': '.',
+      //'sonar.inclusions' : '.' // Entry point of your code
+    }
+  }, () => {}
+);
